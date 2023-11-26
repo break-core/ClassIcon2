b5457580e9b670894fb2b73519f592b06207cce4
riall <71668864+break-core@users.noreply.github.com>
2023-11-26 14:36:15 -0500

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,21 @@
+--[[
+	A modern solution to get Explorer class icons with ease.
+	Created by break-core
+]]
+
+return function(className: string, iconSize: number, studioTheme: "Light" | "Dark"): string
+	--- Variables
+	className = className
+	iconSize = iconSize
+	studioTheme = studioTheme
+
+	if className then
+		--- Get the default sized icon
+		if iconSize == 1 then
+			return `rbxasset://studio_svg_textures/Shared/InsertableObjects/{studioTheme}/Standard/{className}.png`
+			-- Get the 2x or 3x icon
+		elseif iconSize == 2 or 3 then
+			return `rbxasset://studio_svg_textures/Shared/InsertableObjects/{studioTheme}/Standard/{className}@{iconSize}x.png`
+		end
+	end
+end
